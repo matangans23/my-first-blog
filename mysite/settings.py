767f6e33ba097e1599f1d310b15a20ee68d78278
matangans23 <matangans@verizon.net>
2017-07-02 18:50:24 -0400

    changed HTML

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c&e5rpzb5t0-$5sj0+xc*+4vlqkzy&*c)_w_69i@hw00hvz)()'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'matangans.pythonanywhere.com']
 
 
 # Application definition
